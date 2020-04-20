16e9a8c34306d4a9ae5173931399a1117ee36e77
LiYiQin <mslzone@163.com>
2020-04-20 19:02:15 +0800

    加入碰、杠指示器。

--- assets/Script/prefab/game/mjCardGroup.ts
+++ assets/Script/prefab/game/mjCardGroup.ts
@@ -1,4 +1,4 @@
-import { CardId, CardGroupType, Direction } from '../../model/game/concept';
+import { CardId, CardGroupType, Direction, ScreenDirection } from '../../model/game/concept';
 
 const { ccclass, property } = cc._decorator;
 
@@ -7,6 +7,18 @@ const { ccclass, property } = cc._decorator;
  */
 @ccclass
 export default class MjCardGroup extends cc.Component {
+
+  /**
+   * 来源方位指示器节点。
+   */
+  @property(cc.Node)
+  indicatorNode: cc.Node = null;
+
+  /**
+   * 指示器初始旋转角度。
+   */
+  @property(cc.Integer)
+  indicatorSDir = ScreenDirection.Left;
 
   /**
    * 牌花精灵数组。
@@ -19,12 +31,6 @@ export default class MjCardGroup extends cc.Component {
    */
   @property(cc.SpriteFrame)
   cardFaceSpriteFrames: cc.SpriteFrame[] = [];
-
-  /**
-   * 来源方位指示器节点。
-   */
-  @property(cc.Node)
-  indicatorNode: cc.Node = null;
 
   /**
    * 牌组类型。
@@ -130,6 +136,164 @@ export default class MjCardGroup extends cc.Component {
     for (let s of this.cardFaceSprites) {
       s.spriteFrame = sf;
     }
-    //TODO 设置指示器。
+
+    // 设置指示器。
+    if (this.indicatorNode) {
+      if (ownerDir === fromDir) {
+        this.indicatorNode.active = false;
+      } else {
+        this.indicatorNode.active = true;
+        switch (ownerDir) {
+          case Direction.East:
+            switch (fromDir) {
+              case Direction.North:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+              case Direction.West:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = 180;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = -180;
+                }
+                break;
+              case Direction.South:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = -180;
+                }
+                break;
+            }
+            break;
+          case Direction.North:
+            switch (fromDir) {
+              case Direction.East:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 180;
+                }
+                break;
+              case Direction.West:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -180;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+              case Direction.South:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = 180;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+            }
+            break;
+          case Direction.West:
+            switch (fromDir) {
+              case Direction.East:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = -180;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+              case Direction.North:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = -180;
+                }
+                break;
+              case Direction.South:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -180;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 0;
+                }
+                break;
+            }
+            break;
+          case Direction.South:
+            switch (fromDir) {
+              case Direction.East:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -180;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+              case Direction.North:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = -180;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+              case Direction.West:
+                if (this.indicatorSDir === ScreenDirection.Bottom) {
+                  this.indicatorNode.angle = 90;
+                } else if (this.indicatorSDir === ScreenDirection.Top) {
+                  this.indicatorNode.angle = -90;
+                } else if (this.indicatorSDir === ScreenDirection.Left) {
+                  this.indicatorNode.angle = 0;
+                } else if (this.indicatorSDir === ScreenDirection.Right) {
+                  this.indicatorNode.angle = 90;
+                }
+                break;
+            }
+            break;
+        }
+      }
+    }
   }
 }
